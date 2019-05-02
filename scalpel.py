360f709172f193bf3e0f416b928b3419fbb1fa92
DPS2004 <iheartscratch@gmail.com>
2019-05-02 18:38:56 -0400

    Added clone hero support. See credits for more information.

--- scalpel.py
+++ scalpel.py
@@ -68,8 +68,9 @@ while 0 == 0:
     print("1. Effect repeater")
     print("2. Grade calculator")
     print("3. GIF exporter")
-    print("4. Credits")
-    print("5. Exit")
+    print("4. Clone hero BPM converter")
+    print("5. Credits")
+    print("6. Exit")
     selection = input("Please enter a number: ")
     if selection == "1":
         clear()
@@ -103,7 +104,7 @@ while 0 == 0:
                 outputfile.write(effecttwo)
             beatone = beatone + effectdistance
             i = i + 1
-        print("done! check output.txt")
+        print("Done! Open output.txt and copy the contents to the .rdlevel file")
         waitforkey()
         clear()
 
@@ -114,8 +115,7 @@ while 0 == 0:
         print("B rank: " + str(round(hitcount*0.2)) + " misses")
         print("C rank: " + str(round(hitcount*0.3)) + " misses")
         print("D rank: " + str(round(hitcount*0.4)) + " misses")
-        print("E rank: " + str(round(hitcount*0.5)) + " misses")
-        print("F rank: " + str(round(hitcount*0.6)) + " misses")
+        print("F rank: " + str(round(hitcount*0.5)) + " misses")
         waitforkey()
         clear()
     if selection == "3":
@@ -152,13 +152,58 @@ while 0 == 0:
                 beatone = beatone + bpf
                 itwo = itwo + 1
             i = i + 1
-        print("done! Check output.txt in the output folder.")
+        print("Done! Open output.txt in the output folder and copy the contents to the .rdlevel file")
         waitforkey()
         clear()
     if selection == "4":
+        infile = input("Enter clone hero file: ")
+
+        spacing = int(input("In CH - How long is one beat in position: ")) * 2
+        crochet = int(input("In RD - What is your crochet length: "))
+        space = int(input("How many spaces for indentation? (if unsure, put 2)"))
+        outfilewrite = open("output.txt", 'w')
+        print("Processing...")
+
+
+        try:
+            with open(infile) as file:
+                syncfound = False
+                for line in file:
+                    line = line[:-1]  # Remove \n from end of each line
+
+                    if line != "[SyncTrack]" and syncfound == False:
+                        continue
+                    elif syncfound == False:  # Skip until the Sync Track section
+                        file.readline()
+                        file.readline()
+                        file.readline()  # Don't need the original bpm or time sig
+                        syncfound = True
+                        continue
+                    if line == "}":  # End at the end of the bpm section
+                        break
+
+                    info = line.split(' ')
+                    if info[space + 2] == "TS":  # Don't worry about time signature changes
+                        continue
+
+                    bpm = str(float(info[space + 3]) / 1000)  # math
+                    bar = str(int(int(info[space]) / spacing / crochet) + 1)
+                    beat = str(float(int(info[space]) / spacing % crochet) + 1)
+                    rdstr = '        { "bar": ' + bar + ', "beat": ' + beat + ', "y": 0, "type": "SetBeatsPerMinute", "beatsPerMinute": ' + bpm + ' },\n'
+                    outfilewrite.write(rdstr)
+                outfilewrite.close()
+            print("Done! Open output.txt and copy the contents to the .rdlevel file")
+            waitforkey()
+            clear()
+        except:
+            print("CH file not found")
+            waitforkey()
+            clear()
+    if selection == "5":
         clear()
-        print("Made with <3 by DPS2004#5143")
+        print("Effect repeater, Gif importer, and grade calculator were made with <3 by DPS2004")
+        print("Clone hero bpm converter made by Not El Donte and Klyzx")
         waitforkey()
         clear()
-    if selection == "5":
+    if selection == "6":
         break
